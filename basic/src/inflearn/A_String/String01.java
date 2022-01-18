05c4f9a31ed76f6a9ffb4432212c12a77ea135fc
choahkimm <choahk.dev@gmail.com>
2022-01-18 23:59:36 +0900

    test

---
+++ basic/src/inflearn/A_String/String01.java
@@ -0,0 +1,18 @@
+package inflearn.A_String;
+
+import java.util.Scanner;
+
+public class String01 {
+    public int solution(String str, char t){
+        int answer=0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        System.out.print(kb);
+    }
+
+}
