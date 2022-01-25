02b82d80a9b341eec2e24eb6f0800f8b2e52a01e
choahkimm <choahk.dev@gmail.com>
2022-01-25 22:35:10 +0900

    22/01/25 화

---
+++ basic/src/inflearn/A_String/Template.java
@@ -0,0 +1,17 @@
+package inflearn.A_String;
+
+import java.util.Scanner;
+
+public class Template {
+    public static String solution(String str){
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(solution(str));
+    }
+}
