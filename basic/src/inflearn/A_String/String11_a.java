e1cf4062a57e0dba5a9b2baeac81efdb1ca3bb22
choahkimm <choahk.dev@gmail.com>
2022-01-30 21:44:22 +0900

    22/01/30 일

---
+++ basic/src/inflearn/A_String/String11_a.java
@@ -0,0 +1,26 @@
+package inflearn.A_String;
+
+import java.util.Scanner;
+
+public class String11_a {
+    public static String solution(String s){
+        String answer="";
+        s=s+" ";
+        int cnt=1;
+        for(int i=0; i<s.length()-1; i++){
+            if(s.charAt(i)==s.charAt(i+1)) cnt++;
+            else{
+                answer+=s.charAt(i);
+                if(cnt>1) answer+=String.valueOf(cnt);
+                cnt=1;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        String str=kb.next();
+        System.out.println(solution(str));
+    }
+}
