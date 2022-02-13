b430bbcf369e4d1e456177763af1ae4a657d50b6
choahkimm <choahk.dev@gmail.com>
2022-02-13 23:41:20 +0900

    22/02/13 일

--- basic/src/inflearn/B_Array/Array07.java
+++ basic/src/inflearn/B_Array/Array07.java
@@ -3,6 +3,7 @@ package inflearn.B_Array;
 import java.util.Scanner;
 
 public class Array07 {
+    // 7. 점수계산
     public static int[] solution(int n, int[] arr){
         int[] answer = new int[n];
         answer[0] = arr[0];
@@ -30,7 +31,6 @@ public class Array07 {
         for(int i=0; i<n; i++){
             arr[i] = sc.nextInt();
         }
-        //System.out.println(Arrays.toString(solution(n, arr)));
 
         int sum=0;
         for(int i : solution(n,arr)){
