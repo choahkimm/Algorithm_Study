d650899cd41f0f600c37cfc030f2776f1aa98153
choahkimm <choahk.dev@gmail.com>
2022-02-23 23:49:40 +0900

    22/02/23 수

--- basic/src/inflearn/B_Array/Array11_a.java
+++ basic/src/inflearn/B_Array/Array11_a.java
@@ -1,4 +1,37 @@
 package inflearn.B_Array;
 
+import java.util.Scanner;
+
 public class Array11_a {
+    public static int solution(int n, int[][] arr){
+        int answer=0, max=0;
+        for(int i=1; i<=n; i++){
+            int cnt=0;
+            for(int j=1; j<=n; j++){
+                for(int k=1; k<=5; k++){
+                    if(arr[i][k]==arr[j][k]){
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if(cnt>max){
+                max=cnt;
+                answer=i;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args){
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int[][] arr=new int[n+1][6];
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=5; j++){
+                arr[i][j]=kb.nextInt();
+            }
+        }
+        System.out.print(solution(n, arr));
+    }
 }
