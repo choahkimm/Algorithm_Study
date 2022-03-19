166a9b9f78f0c3664647b712f38d22d6038779bf
choahkimm <choahk.dev@gmail.com>
2022-03-19 17:31:13 +0900

    22/03/19 토

--- basic/src/inflearn/C_TowPointers_SlidingWindow/C05.java
+++ basic/src/inflearn/C_TowPointers_SlidingWindow/C05.java
@@ -1,12 +1,41 @@
 package inflearn.C_TowPointers_SlidingWindow;
 
+import java.util.Scanner;
+
 public class C05 {
     public static void main(String[] args) {
+        //5. 연속된 자연수의 합
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // lt rt
+        // 1  2  3 4 5 6 7 8 9 10 11 12 13 14 15
+
+        // 풀이 - two pointer?
+        int[] arr = new int[n];
+        int pl=1, pr=2, sum=pl, answer=0;
+        while (pr<n){
+            sum += pr;
+            if(sum==n){
+                answer++;
+            }
+            else {
+                while (sum>=n){
+                    sum -= pl;
+                    pl++;
+                    if(sum==n) answer++;
+                }
+            }
+            pr++;
+
+        }
+        System.out.println(answer);
+        // 풀이 - sliding window? - 윈도우 크기 2개일때, 3개일 때, 4개일때...n개..? 떼잉.
 
     }
 }
 /*
 5. 연속된 자연수의 합
+
 설명
 
 N입력으로 양의 정수 N이 입력되면 2개 이상의 연속된 자연수의 합으로 정수 N을 표현하는 방법의 가짓수를 출력하는 프로그램을 작성하세요.
--- basic/src/inflearn/C_TowPointers_SlidingWindow/C06.java
+++ basic/src/inflearn/C_TowPointers_SlidingWindow/C06.java
@@ -3,5 +3,6 @@ package inflearn.C_TowPointers_SlidingWindow;
 public class C06 {
     public static void main(String[] args) {
 
+
     }
 }
