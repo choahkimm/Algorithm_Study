2e6f418977f4693eb23f9e09c42eda3212d6c372
choahkimm <choahk.dev@gmail.com>
2022-03-27 23:59:44 +0900

    22/03/25 금

---
+++ basic/src/inflearn/D_HashMap_HashSet_TreeSet/Hash_prctice.java
@@ -0,0 +1,14 @@
+package inflearn.D_HashMap_HashSet_TreeSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hash_prctice {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("하나",1);
+        map.put("둘",2);
+        map.put("셋",3);
+        System.out.println(map);
+    }
+}
