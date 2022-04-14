c8f14c1ef5bd730506af05ed112086d20ab11944
choahkimm <choahk.dev@gmail.com>
2022-04-14 23:53:56 +0900

    22/04/14

---
+++ basic/src/doitjava/basic/B004.java
@@ -0,0 +1,17 @@
+package doitjava.basic;
+
+public class B004 {
+    // 03-2 구간 합
+    // 004.구간 합 구하기2 (백준 온라인 저지 11660)
+
+    // 1단계 - 문제 분석
+    //      1차원 -> 2차원 된 것이 포인트 ( D[x][y] )
+    // 2단계 - 손으로 풀기
+    // 3단계 - 슈도코드
+    // 4단계 - 코드 구현
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- basic/src/doitjava/basic/test.java
+++ basic/src/doitjava/basic/test.java
@@ -2,3 +2,14 @@ package doitjava.basic;
 
 public class test {
 }
+
+
+/*
+    연습 순서
+
+    1단계 - 문제 분석
+    2단계 - 손으로 풀기
+    3단계 - 슈도코드
+    4단계 - 코드 구현
+
+ */
