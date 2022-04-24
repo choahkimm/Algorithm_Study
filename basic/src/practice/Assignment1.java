eb9be7557feeea8706e9bbb6ea0a9b0c8f7c556b
choahkimm <choahk.dev@gmail.com>
2022-04-25 00:57:06 +0900

    assinment1

---
+++ basic/src/practice/Assignment1.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        for(int i=1; i<11; i++){
+            System.out.println(i);
+        }
+
+        System.out.print("JOAH");
+    }
+}
