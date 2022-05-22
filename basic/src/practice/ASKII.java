be1059e566dcb36ab7bca1d2622037fc03176cd1
choahkimm <choahk.dev@gmail.com>
2022-05-22 23:15:17 +0900

    22/05/22 data type study

---
+++ basic/src/practice/ASKII.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class ASKII {
+    public static void main(String[] args) {
+        // printing letters to numbers
+
+        String upper = "A";
+        char letter = upper.charAt(0); // Convert 0th String -> char type
+        int num = letter; // char -> int
+        System.out.println(num);
+    }
+}
+
