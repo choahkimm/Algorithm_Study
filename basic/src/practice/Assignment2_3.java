e50a7df7a7dd82b2d71963799d21518855a5069c
choahkimm <choahk.dev@gmail.com>
2022-05-29 23:35:14 +0900

    22/05/29 assinments2

---
+++ basic/src/practice/Assignment2_3.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class Assignment2_3 {
+    public static void main(String[] args) {
+
+        // ternary conditional statement (3항 연산자)
+        // booleanExpression ? expression1 : expression2
+
+
+        // if-else statement
+        int a = 10;
+        if(a==2) System.out.println("true");
+        else System.out.println("false");
+
+        // ternary conditional statment
+        System.out.println((a==10)? "true": "false");
+    }
+}
