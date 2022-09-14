3c13a8671b96e28d1e915128d663b6cc17c6ddf3
choahkimm <choahk.dev@gmail.com>
2022-09-14 23:56:50 +0900

    22/09/12 영리한 프로그래밍을 위한 알고리즘 강좌 (순환)

--- basic/src/inflearn_concept/Recursion_ex3.java
+++ basic/src/inflearn_concept/Recursion_ex3.java
@@ -18,4 +18,18 @@ public class Recursion_ex3 {
     }
     // 이 함수의 미션은 data[0]에서 data[n-1] 사이에서 target을 검색하는 것이다.
     // 하지만 검색 구간의 시작 인덱스 0은 보통 생략한다. 즉 암시적 매개변수이다.
+
+
+    // ex2) 매개변수의 명시화 : 순차 탐색
+    // 이 함수의 미션은 data[begin]에서 data[end] 사이에서 target을 검색한다.
+    // 즉, 검색구간의 시작점을 명시적(explicit)으로 지정한다.
+    int search2(int [] data, int begin, int end, int target){
+        if(begin>end)
+            return -1;
+        else if(target==data[begin])
+            return begin;
+        else
+            return search2(data, begin+1, end, target);
+        // recursion을 쓰려면,, 가능하면 명시적으로 매개변수를 지정해주는 것이 좋음
+    }
 }
