0ae10339d9935c06d3c95a806532ffac62d02a43
choahkimm <choahk.dev@gmail.com>
2022-09-20 00:06:10 +0900

    22/09/19 영리한 프로그래밍을 위한 알고리즘 강좌 (순환)

--- basic/src/inflearn_concept/Recursion_ex3.java
+++ basic/src/inflearn_concept/Recursion_ex3.java
@@ -56,5 +56,19 @@ public class Recursion_ex3 {
     }
 
     // 이진검색 알고리즘 Binary Search
+    public static int binarySearch(String[] items, String target, int begin, int end ){
+        if(begin>end)
+            return -1;
+        else {
+            int middle = (begin+end)/2;
+            int compResult = target.compareTo(items[middle]);
+            if(compResult == 0)
+                return middle;
+            else if(compResult<0)
+                return binarySearch(items, target, begin, middle-1);
+            else
+                return binarySearch(items, target, middle+1, end);
 
+        }
+    }
 }
