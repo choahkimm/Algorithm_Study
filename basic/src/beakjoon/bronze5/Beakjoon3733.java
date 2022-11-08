97b0151628abc5eabea16baa883eb2bddbe0fd68
choahkimm <choahk.dev@gmail.com>
2022-11-08 09:58:39 +0900

    Beakjoon Bronze5

--- basic/src/beakjoon/bronze5/Beakjoon3733.java
+++ basic/src/beakjoon/bronze5/Beakjoon3733.java
@@ -1,7 +1,10 @@
 package beakjoon.bronze5;
 
-public class Beakjoon3733 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Beakjoon3733 {
+    // 백준 3733. Shares
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
     }
 }
