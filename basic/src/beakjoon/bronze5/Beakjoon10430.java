1eb442c6645cb3cd1b5bbdde2b9b6fc90341c8a8
choahkimm <choahk.dev@gmail.com>
2022-11-11 00:34:33 +0900

    Beakjoon Bronze5

---
+++ basic/src/beakjoon/bronze5/Beakjoon10430.java
@@ -0,0 +1,18 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class Beakjoon10430 {
+    // 백준 10430. 나머지
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+    }
+}
