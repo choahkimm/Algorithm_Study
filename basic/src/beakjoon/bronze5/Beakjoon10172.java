db2a42feff04f72bb59c36af47bcdc389208048d
choahkimm <choahk.dev@gmail.com>
2022-11-10 23:56:36 +0900

    Beakjoon Bronze5

---
+++ basic/src/beakjoon/bronze5/Beakjoon10172.java
@@ -0,0 +1,14 @@
+package beakjoon.bronze5;
+
+public class Beakjoon10172 {
+    // 백준 10172. 개
+    public static void main(String[] args) {
+        System.out.println(
+                "|\\_/|\n" +
+                        "|q p|   /}\n" +
+                        "( 0 )\"\"\"\\\n" +
+                        "|\"^\"`    |\n" +
+                        "||_/=\\\\__|"
+        );
+    }
+}
