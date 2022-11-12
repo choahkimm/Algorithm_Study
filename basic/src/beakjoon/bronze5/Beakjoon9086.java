5b27528528ef60f09515425bc84ab1d25b35cc74
choahkimm <choahk.dev@gmail.com>
2022-11-13 00:49:14 +0900

    Beakjoon Bronze5

--- basic/src/beakjoon/bronze5/Beakjoon9086.java
+++ basic/src/beakjoon/bronze5/Beakjoon9086.java
@@ -1,4 +1,21 @@
 package beakjoon.bronze5;
 
+import java.util.Scanner;
+
 public class Beakjoon9086 {
+    // 백준 9086. 문자열
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int testCount = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<testCount; i++){
+            String s = sc.next();
+            sb.append(s.charAt(0));
+            sb.append(s.charAt(s.length()-1)+"\n");
+        }
+        System.out.println(sb);
+    }
 }
+
