54fc8be73fbeaf7699b755b4acf2d39fc9b901c2
choahkimm <choahk.dev@gmail.com>
2022-11-15 00:01:57 +0900

    Beakjoon Bronze5

---
+++ basic/src/beakjoon/bronze5/Beakjoon16394.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class Beakjoon16394 {
+    // 백준 16394. 홍익대학교
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n-1946);
+    }
+}
