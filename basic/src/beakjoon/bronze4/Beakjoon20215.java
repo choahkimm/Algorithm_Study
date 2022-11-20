22369c9f4df056600e4ebc3907a91a8068ac8ba7
choahkimm <choahk.dev@gmail.com>
2022-11-20 13:53:05 +0900

    Beakjoon Bronze4

--- basic/src/beakjoon/bronze4/Beakjoon20215.java
+++ basic/src/beakjoon/bronze4/Beakjoon20215.java
@@ -1,5 +1,14 @@
 package beakjoon.bronze4;
 
+import java.util.Scanner;
+
 public class Beakjoon20215 {
-    //
+    // 백준 20215. Cutting Corners
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt(), w = sc.nextInt();
+        double cutting_corners = Math.sqrt(h*h + w*w);
+        // 제곱근(루트 구하기) Math.sqrt()
+        System.out.println(h+w - cutting_corners);
+    }
 }
