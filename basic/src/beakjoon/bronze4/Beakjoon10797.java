cc705b0ae15d9606a93927bb54e039a5ccb13aa4
choahkimm <choahk.dev@gmail.com>
2022-11-23 16:43:55 +0900

    Beakjoon Bronze4

---
+++ basic/src/beakjoon/bronze4/Beakjoon10797.java
@@ -0,0 +1,24 @@
+package beakjoon.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon10797 {
+    // 백준 10797. 10부제
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int [] arr = new int [5];
+        int cnt = 0;
+        for(int i:arr){
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(n==arr[i]) cnt++;
+        }
+        System.out.println(cnt);
+
+
+    }
+}
---
+++ basic/src/beakjoon/bronze4/Beakjoon14924.java
@@ -0,0 +1,20 @@
+package beakjoon.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon14924 {
+    // 백준 14924. 폰 노이만과 파리
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int S = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        System.out.println((D/(S*2))*T);
+    }
+}
