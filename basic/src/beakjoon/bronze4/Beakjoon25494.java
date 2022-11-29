33bc554bc7f12bb712b20af802ccae22cd8ed1c2
choahkimm <choahk.dev@gmail.com>
2022-11-29 20:38:02 +0900

    Beakjoon Bronze4

---
+++ basic/src/beakjoon/bronze4/Beakjoon25494.java
@@ -0,0 +1,25 @@
+package beakjoon.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Beakjoon25494 {
+    // 백준 25494. 단순한 문제 (Small)
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<t; i++){
+            StringTokenizer st =new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int [] nums = {a,b,c};
+            Arrays.sort(nums);
+            System.out.println(nums[0]);
+        }
+    }
+}
