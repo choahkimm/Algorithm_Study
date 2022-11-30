1cc24711c364e91cc8f64449234b3c761e1e9b43
choahkimm <choahk.dev@gmail.com>
2022-11-30 16:20:45 +0900

    Beakjoon Bronze4

--- basic/src/beakjoon/bronze4/Beakjoon25704.java
+++ basic/src/beakjoon/bronze4/Beakjoon25704.java
@@ -11,16 +11,14 @@ public static void main(String[] args) throws IOException {
     int n = Integer.parseInt(br.readLine());
     int p = Integer.parseInt(br.readLine());
 
-    int cnt = n/5;
     int minP = p;
-    for(int i=1; i<=cnt; i++ ){
-        if(i*5>n) break;
-        switch (i) {
-            case 1 : minP = Math.min(minP,p-500); break;
-            case 2 : minP = Math.min(minP, (int)(p*0.9)); break;
-            case 3 : minP = Math.min(minP, p-2000); break;
-            case 4 : minP = Math.min(minP, (int)(p*0.75)); break;
-        }
+    switch (n/5) { // n 입력은 6까지니까
+            case 1 : minP = Math.min(minP, p-500); break;
+            case 2 : minP = Math.min(p-500, (int)(p*0.9)); break;
+            case 3 : minP = Math.min((int)(p*0.9), p-2000); break;
+            case 4 : minP = Math.min(p-2000, (int)(p*0.75)); break;
+            case 5 : minP = Math.min(p-2000, (int)(p*0.75)); break;
+            case 6 : minP = Math.min(p-2000, (int)(p*0.75)); break;
     }
 
     System.out.println(minP<0? 0 : minP);
@@ -28,7 +26,7 @@ public static void main(String[] args) throws IOException {
     }
 }
 
-// 다른 사람 참고해서 푼 문제
+// 다른 풀이 참고
 // https://nahwasa.com/entry/%EC%9E%90%EB%B0%94-%EB%B0%B1%EC%A4%80-25704-%EC%B6%9C%EC%84%9D-%EC%9D%B4%EB%B2%A4%ED%8A%B8-java
 
 
