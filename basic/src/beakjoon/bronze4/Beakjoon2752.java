39ebf27e19e1c073e0cffa8755fa25932b6a4a7b
choahkimm <choahk.dev@gmail.com>
2022-12-06 09:59:30 +0900

    beakjoon bronze 4

---
+++ basic/src/beakjoon/bronze4/Beakjoon2752.java
@@ -0,0 +1,21 @@
+package beakjoon.bronze4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Beakjoon2752 {
+    // 백준 2752. 세수정
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int [] arr = {a,b,c};
+        Arrays.sort(arr); // 오름차순 정렬
+        System.out.println(arr[0]+" "+arr[1]+" "+arr[2]);
+    }
+}
