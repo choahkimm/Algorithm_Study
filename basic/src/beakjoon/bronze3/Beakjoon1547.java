99c749bfa20e3bae00599147614736eb038302ce
choahkimm <choahk.dev@gmail.com>
2023-01-10 23:35:10 +0900

    beakjoon bronze 3

---
+++ basic/src/beakjoon/bronze3/Beakjoon1547.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Beakjoon1547 {
+    // 백준 1547. 공
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
