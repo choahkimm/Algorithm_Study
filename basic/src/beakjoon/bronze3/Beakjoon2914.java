3424e2622bc3e0189713f7ab140679b00f359956
choahkimm <choahk.dev@gmail.com>
2023-01-12 23:58:59 +0900

    beakjoon bronze 3

---
+++ basic/src/beakjoon/bronze3/Beakjoon2914.java
@@ -0,0 +1,18 @@
+package beakjoon.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon2914 {
+    // 백준 2914. 저작권
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()); // 수록된 곡
+        int b = Integer.parseInt(st.nextToken()); // 평균값
+
+        System.out.println();
+    }
+}
