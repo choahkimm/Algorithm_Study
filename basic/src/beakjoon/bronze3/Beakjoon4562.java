8061f97645adb90673147c8ce5b3be3b847dce56
choahkimm <choahk.dev@gmail.com>
2023-01-29 23:18:35 +0900

    Beakjoon Bronze 3

---
+++ basic/src/beakjoon/bronze3/Beakjoon4562.java
@@ -0,0 +1,24 @@
+package beakjoon.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Beakjoon4562 {
+    // Beakjoon 4562. No Brainer
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            if(x<y) System.out.println("NO BRAINS");
+            else System.out.println("MMM BRAINS");
+
+        }
+
+    }
+}
