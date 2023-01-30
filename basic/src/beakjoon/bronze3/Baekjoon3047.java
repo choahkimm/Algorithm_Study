9a45fdc43734c83ff9da7efcd43af6cf6ca0d7a1
choahkimm <choahk.dev@gmail.com>
2023-01-30 23:39:25 +0900

    Beakjoon Bronze 3

---
+++ basic/src/beakjoon/bronze3/Baekjoon3047.java
@@ -0,0 +1,10 @@
+package beakjoon.bronze3;
+
+import java.io.BufferedReader;
+
+public class Baekjoon3047 {
+    // 백준 3047. ABC
+    public static void main(String[] args) {
+        BufferedReader br
+    }
+}
