3c1411d85a7a3bda97af9685178cc6437e6b32b4
choahkimm <choahk.dev@gmail.com>
2023-02-22 00:07:17 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beakjoon5073.java
+++ basic/src/beakjoon/bronze3/Beakjoon5073.java
@@ -11,18 +11,24 @@ public class Beakjoon5073 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         while (true){
-           String input = br.readLine();
-           if("0 0 0".equals(input)) break;
+            String input = br.readLine();
+//           if("0 0 0".equals(input)) break;
 
-           StringTokenizer st = new StringTokenizer(input);
-           int a = Integer.parseInt(st.nextToken());
-           int b = Integer.parseInt(st.nextToken());
-           int c = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(input);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
 
-           if(a==b && b==c && a==c) System.out.println("Equilateral");
-           else if(a==b || b==c || a==c) System.out.println("Isosceles");
-           else System.out.println("Scalene");
+            if(a==0 && b==0 && c==0) break;
+
+            if(a == b && b == c && c == a)
+                System.out.println("Equilateral");
+            else if(a + b <= c || a + c <= b || b + c <= a)
+                System.out.println("Invalid");
+            else if(a == b || b == c || c == a)
+                System.out.println("Isosceles");
+            else
+                System.out.println("Scalene");
         }
-
     }
 }
