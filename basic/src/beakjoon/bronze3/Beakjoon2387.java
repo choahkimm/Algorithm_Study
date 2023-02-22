9f9449cd951bb710f7598991dbd5f5341474fee0
choahkimm <choahk.dev@gmail.com>
2023-02-22 14:11:48 +0900

    Beakjoon Bronze 3

---
+++ basic/src/beakjoon/bronze3/Beakjoon2387.java
@@ -0,0 +1,21 @@
+package beakjoon.bronze3;
+
+public class Beakjoon2387 {
+    // 백준 2387. Howl
+    public static void main(String[] args) {
+        System.out.println(
+                "                     .\n" +
+                        "                    / V\\\n" +
+                        "                  / `  /\n" +
+                        "                 <<   |\n" +
+                        "                 /    |\n" +
+                        "               /      |\n" +
+                        "             /        |\n" +
+                        "           /    \\  \\ /\n" +
+                        "          (      ) | |\n" +
+                        "  ________|   _/_  | |\n" +
+                        "<__________\\______)\\__)"
+
+        );
+    }
+}
