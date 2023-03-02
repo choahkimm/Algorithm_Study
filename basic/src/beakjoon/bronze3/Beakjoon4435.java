9a9fd57d618d74d42e2f30e4670fe16ead5a9f0f
choahkimm <choahk.dev@gmail.com>
2023-03-03 00:19:59 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beakjoon4435.java
+++ basic/src/beakjoon/bronze3/Beakjoon4435.java
@@ -10,13 +10,17 @@ public class Beakjoon4435 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+
+        int rate[][] = { { 1, 2, 3, 3, 4, 10 }, { 1, 2, 2, 2, 3, 5, 10 } };
+
         for(int i=0; i<t; i++){
             int armys [] = new int[2];
 
             for(int j=0; j<2; j++){
                 StringTokenizer st = new StringTokenizer(br.readLine());
+                int k=0;
                 while (st.hasMoreTokens()){
-                    armys[j] += Integer.parseInt(st.nextToken());
+                    armys[j] += Integer.parseInt(st.nextToken()) * rate[j][k++];
                 }
             }
             String message = "";
@@ -24,7 +28,7 @@ public class Beakjoon4435 {
             else if(armys[0] < armys[1]) message="Evil eradicates all trace of Good";
             else message = "No victor on this battle field";
 
-            System.out.println("Battle " + i+1 + ": " +message);
+            System.out.println("Battle " + (i+1) + ": " +message);
         }
     }
 }
