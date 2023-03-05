a87d0f6cb7355c3043d82774da9331ef3bc9d8fb
choahkimm <choahk.dev@gmail.com>
2023-03-06 01:47:02 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beakjoon5217.java
+++ basic/src/beakjoon/bronze3/Beakjoon5217.java
@@ -1,5 +1,25 @@
 package beakjoon.bronze3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Beakjoon5217 {
     // 백준 5217. 쌍의 합
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        for(int i=0; i<t; i++){
+            int n = Integer.parseInt(br.readLine());
+            String s = "";
+            for(int j=1; j<=n/2; j++){
+                    int a= j;
+                    int b= n-j;
+                    if(a!=b){
+                        s+= (b-a>n/2)? " "+a+" "+b+"," : " "+a+" "+b;
+                    }
+            }
+            System.out.println("Paris for " + n +":" + s );
+        }
+    }
 }
