7dafab6f7683f2ce7c31592c7a6f8d604ab83afc
choahkimm <choahk.dev@gmail.com>
2023-03-22 23:55:37 +0900

    Beakjoon Bronze 3

---
+++ basic/src/beakjoon/bronze3/Beakjoon5354.java
@@ -0,0 +1,14 @@
+package beakjoon.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Beakjoon5354 {
+    // 백준 5354. J박스
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+    }
+}
