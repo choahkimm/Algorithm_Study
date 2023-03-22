154e27dd513b659f114f5205e073436236071796
choahkimm <choahk.dev@gmail.com>
2023-03-22 23:55:44 +0900

    Beakjoon Bronze 3

---
+++ basic/src/beakjoon/bronze5/Beakjoon14928.java
@@ -0,0 +1,12 @@
+package beakjoon.bronze5;
+
+import java.util.Scanner;
+
+public class Beakjoon14928 {
+    // 백준 14928. 큰 수(BIG)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n % 20000303);
+    }
+}
