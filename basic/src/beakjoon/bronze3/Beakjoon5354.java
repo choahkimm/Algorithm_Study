3a8e72bca8dc78a5760c6e1ce855454adc070d1b
choahkimm <choahk.dev@gmail.com>
2023-03-24 19:13:19 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beakjoon5354.java
+++ basic/src/beakjoon/bronze3/Beakjoon5354.java
@@ -9,6 +9,29 @@ public class Beakjoon5354 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
+        for(int i=0; i<testCase; i++){
+            int n = Integer.parseInt(br.readLine());
+            StringBuilder sb = new StringBuilder();
 
+            for(int j=0; j<n; j++){
+                sb.append("#");
+            }
+            System.out.println(sb);
+
+            StringBuilder sb2 = new StringBuilder();
+            sb2.append("#");
+            for(int j=0; j<n-2; j++){
+                sb2.append("J");
+            }
+            sb2.append("#");
+
+
+            for(int j=0; j<n-2; j++){
+                System.out.println(sb2);
+            }
+            System.out.println(sb);
+
+            System.out.println();
+        }
     }
 }
