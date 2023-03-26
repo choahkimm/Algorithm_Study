79f2d8f05c67f2984e4eccfd13551b1301ba5691
choahkimm <choahk.dev@gmail.com>
2023-03-27 00:01:51 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beakjoon4922.java
+++ basic/src/beakjoon/bronze3/Beakjoon4922.java
@@ -1,8 +1,23 @@
 package beakjoon.bronze3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Beakjoon4922 {
     // 백준 4922. Walk Like an Egyptian
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true){
+            int n = Integer.parseInt(br.readLine());
+            if(n==0) break;
 
+            System.out.print(n +" => ");
+            int sum = 0;
+            for(int i=0; i<n; i++){
+                sum += 2*i;
+            }
+            System.out.println(1+sum);
+        }
     }
 }
