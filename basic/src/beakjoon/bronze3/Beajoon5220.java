7c7e093dd9b1ef437fb1c3fefe4c9b20650db7e7
choahkimm <choahk.dev@gmail.com>
2023-03-30 18:01:57 +0900

    Beakjoon Bronze 3

--- basic/src/beakjoon/bronze3/Beajoon5220.java
+++ basic/src/beakjoon/bronze3/Beajoon5220.java
@@ -7,23 +7,45 @@ import java.util.StringTokenizer;
 
 public class Beajoon5220 {
     // 백준 5220. Error Detection
+    private static boolean solveErrorDetection(int n, int p) {
+        boolean valid = false;
+
+        /* ------------------- INSERT CODE HERE ---------------------*/
+        int r = 0;
+
+        String binaryString = Integer.toBinaryString(n);
+
+        for(char c : binaryString.toCharArray()){
+            r += c -'0';
+        }
+
+        if((r%2==0 && p==0)||(r%2==1 && p==1)) {
+            valid = true;
+        }else {
+            valid = false;
+        }
+        /* -------------------- END OF INSERTION --------------------*/
+
+        return valid;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        for(int i=0; i<testCase; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int p = Integer.parseInt(st.nextToken());
 
-            int r = 0;
-            while (true){
-                r+= n%2;
-                n= n/2;
-                if(n==1) break;
+        int numCases = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < numCases; i++)
+        {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int value = Integer.parseInt(st.nextToken());
+            int checkbit = Integer.parseInt(st.nextToken());
+
+            if (solveErrorDetection(value, checkbit)) {
+                System.out.println("Valid");
             }
-            if((r%2==0 && p==0)||(r%2==1 && p==1)) {
+            else {
                 System.out.println("Corrupt");
-            }else System.out.println("Valid");
+            }
         }
     }
 }
